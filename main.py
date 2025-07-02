e4d75af1c6f253a8aafa766b960b73618b7c07d4
HubertMarczuk <dziabongjp@gmail.com>
2025-07-02 21:53:00 +0200

    Added cuingt and reducing photo

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+from PIL import Image
+
+def preprocess_image(input_path):
+    img = Image.open(input_path)
+    img = img.convert("RGB")
+    
+    img.thumbnail((9999, 256), Image.LANCZOS)
+    
+    width, height = img.size
+    left = (width - 256)/2
+    top = (height - 256)/2
+    right = (width + 256)/2
+    bottom = (height + 256)/2
+    img = img.crop((left, top, right, bottom))
+    
+    img.show()
+
+path = "Master-s-thesis/photos/Audi/IMG_20250311_104415.jpg"
+preprocess_image(path)
